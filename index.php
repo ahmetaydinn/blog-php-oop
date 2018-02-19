fe9fccfdc9afefb6cb8bada62ce9054cb280fdc7
VictorLeite <victor.leite@gmail.com>
2018-02-19 14:19:41 -0300

    Autoload app classes using composer autoloader

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 session_start();
 
 require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . '/autoload.php');
 
 $config = require(__DIR__ . '/config/main.php');
 $app = \app\components\Application::getInstance($config);
